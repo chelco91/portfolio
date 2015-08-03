be8e0a66de533c81a548cfed3115f14ad4080d15
Christopher Collopy <chelco91@gmail.com>
2015-08-02 23:31:36 -0600

    minor change to milestone-2.php

--- documentation/milestone-2.php
+++ documentation/milestone-2.php
@@ -56,63 +56,40 @@
 				</nav>
 			</div>
 
-			<!--begin main content panel-->
-			<div class="container">
-				<main class="content-main col-xs-12 col-md-12">
-					<div class="row byline">
-						<div class="col-md-4">
-							Created By: Rochelle Lewis<br>
-							Created For: DDC Bootcamp, P.W. Project
-						</div>
-						<div class="col-md-4">
-							Date: 01/23/2015<br>
-							URL: http://rochellelewis.com
-						</div>
-					</div>
-					<h2>Introduction</h2>
-					<p>The following document outlines the User Experience and Interaction Design for the proposed “Rochelle Lewis Personal Website Project”. This project is a professional portfolio website with a focus on front-end web development. This site is designed with a professional audience in mind, and is targeted to individuals within the web development and I.T. community nationwide. Target audience to include: startup founders, hiring managers, recruiters, and peers. The primary goal of this site is for users of the site to initiate contact in regards to employment and collaboration opportunities through the contact form, and/or the connected social media profiles. The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.</p>
-					<h2>Persona</h2>
-					<p><strong>Name: </strong>David B. McCracken<br><strong>Age: </strong>34</p>
-					<p><strong>Profession: </strong>Co-Founder and CTO of FashionGun.com, a startup that is looking to hire a full-time, in-house Front-End Web Developer. They have just received their first round of VC seed funding. Their product is an online collaboration and project management tool marketed to the fashion industry.</p>
-					<p><strong>Technology: </strong>Mac power user. Uses a Mac Pro Desktop with a 30” cinema display while in the office, a MacBook Pro laptop at home, and an iPhone 6 with an unlimited 4G data connection.</p>
-					<p><strong>Attitudes and Behaviors: </strong>Short on time, overworked, and has a full social schedule in addition. David will not spend more than 3 minutes max looking at anyone’s portfolio site, and only about 30 seconds on a resume. David is a huge social media user, and seeks potential candidates through tech and fashion community networking events, LinkedIn, and Twitter.</p>
-					<p><strong>Frustrations and Needs: </strong>David needs to hire someone with top-tier skills who is willing to start at a mid-level salary. David is also looking for someone who is a good personality and culturural fit for FashionGun as well. FashionGun is still small, and can only offer limited benefits at this time, but will include equity options as part of the compensation package. Because David is looking to hire within a very competitive urban market, he is flooded with candidates but is having a hard time finding someone with the right combination of talent, experience, and personality who is willing to accept a lower salary and limited benefits. He must hire within 2 weeks.</p>
-					<p><strong>Goals: </strong>David is only concerned with one thing: Is this person a potential candidate for the FashionGun Web Dev position? Can David, within 3 minutes max, determine whether this person is worthy of a preliminary phone screen regarding the job? David doesn’t like to waste time. David needs to know via a very fast interaction with the site whether or not this potential candidate has the minimum qualifications to warrant a call.</p>
-					<h2>Use Case</h2>
-					<p>David B. McCracken has received a cover letter and resume from myself in regards to the FashionGun Web Dev position. Based on a brief look at my resume package, he has included my name on a list of potential candidates to search for via LinkedIn.</p>
-					<p>He has viewed my LinkedIn profile, and based on the number of connections we have in common, my work history, tweets and endorsements, he has decided to go one step further and check my personal web site for more information.</p>
-					<p>It is 11:25AM on Monday morning, and David has a lunch appointment coming up in half an hour. David would like to get the first round of 10 preliminary phone screens finished between the hours of 1 - 3pm. Time is short, and all of this effort regarding hiring is additional work to be fit in on top of an already busy schedule. In addition to myself, he has 9 other candidates’ sites to look at.</p>
-					<p>David is viewing the site on his desktop computer at the office, with a 30” cinema screen, using Safari. He has 29 other tabs open. From LinkedIn, he links to my personal website which opens in a new window. In the next 3 minutes, here is an outline of David’s interactions with the site:</p>
-					<ol>
-						<li>Lands on Home page. Clicks call to action that navigates to the Portfolio page.</li>
-						<li>Views first three projects (3 of 8) in Portfolio gallery. Finds work satisfactory. Navigates to the “About Me” page via the main navigation in the header.</li>
-						<li>1 minute look at the About Me page. Sees core skills featured in a contrasting side panel. The interaction stops here. David has decided, based on the information provided, that I will be included in the preliminary phone screens. He does not need to navigate away at this point, because my contact info is visible in the footer. David leaves the About Me page open for reference during the preliminary phone call later.</li>
-					</ol>
-					<img class="img-responsive" src="../images/use-case.jpg" alt="use case flow chart">
-					<h2>Proposed Site Map</h2>
-					<p>Site will consist of 4 top-level pages total. No sub-pages or sub-navigation. This will be the Minimum Viable Product.</p>
-					<ul>
-						<li>Home Page</li>
-						<li>About Me</li>
-						<li>Portfolio</li>
-						<li>Contact</li>
-					</ul>
-					<p><strong>Home Page: </strong>A brief introduction that clearly states my name and role as a Front End Developer, and will feature 2 calls to action that will direct the user to the About Me or the Portfolio page.</p>
-					<p><strong>About Me: </strong>This page will detail my professional history, core skill set, and professional highlights. This page will showcase my strengths and personality. I will feature a contrasting sidebar that lists my core skills which will be easily readable at a glance. The tone of voice will be personable and casual, as I am only interested in opportunities with individuals and organizations that I am a good personality fit for. Depending on the length and detail of the content here, the Contact Form may also be incorporated here.</p>
-					<p><strong>Portfolio: </strong>8 - 10 examples of my past work, including my roles and contributions to each project. I want this to be an animated, responsive image gallery.</p>
-					<p><strong>Contact: </strong>This page will contain a simple contact form, my professional email address, phone number, and additional links to my professional social media profiles: LinkedIn, Twitter, GitHub, Dribbble. (These will also be linked to in the footer site wide via icons.) This page may be integrated into the About Me page if content is short.</p>
-					<h2>Proposed Features and Functionality</h2>
-					<ul>
-						<li>Top navigation bar on all content pages, except for home page.</li>
-						<li>Portfolio page with screenshots of projects, containing details overlaid on the image. This will use a jQuery gallery.</li>
-						<li>Contact Form.</li>
-						<li>Contact info and social media links to be featured in the footer on all pages.</li>
-						<li>Iconography using Bootstrap included glyphs.</li>
-						<li>Full size responsive background imagery. Will use jQuery for this.</li>
-						<li>Contact info to be visible on each page in the Footer area.</li>
-					</ul>
-				</main><!--.content-main-->
-			</div>
+			<main class="container">
+				<h2>Persona</h2>
+				<p><strong>Name: </strong>Albert Wilkinson<br><strong>Age: </strong>58</p>
+				<p><strong>Profession: </strong>Dentist who owns a small practice in northern New Mexico. He and his wife have provided dental care to the surrounding area for 28 years. So far Albert has taken to using written methods of documentation. He realizes that before he retires and sells his practice, he will need to digitize all of his dental records. In addition to digital records he would also like a modern web interface. He is looking for some outside help to accomplish this.</p>
+				<p><strong>Technology: </strong>Uses an old timey flip phone for emergencies, because cell service in the area is spotty. At home he's recently been given a new Dell PC that runs Windows 8 and has internet access. Currently at the office he runs a Gateway desktop with Windows 95 that can connect to the internet via dial-up. Needless to say he is also looking for advice on a system upgrade for the office.</p>
+				<p><strong>Frustrations and Needs: </strong>Albert needs to hire someone with good skills who is willing to travel to his location. Albert is also looking for someone who is easygoing and up for the challenges to do the task. Albert can't offer a very high salary, but he can add unlimited dental work as part of the compensation.</p>
+				<p><strong>Goals: </strong>Albert needs a user friendly site when looking for someone to hire, where qualifications and contact info are easily accessible. He can't deal with/understand complicated navigation schemes.</p>
+				<h2>Use Case</h2>
+				<p>Albert started his search by googling new mexico, web design, and technical support. My website is high on the search results due to my implementation of SEO standards. He checks out my web site.</p>
+				<ol>
+					<li>Lands on Home page. Reads a short bio about me.</li>
+					<li>Views some of my projects in Portfolio section. Finds work satisfactory. Navigates to the "Contact Me" section via a side navigation bar on the page.</li>
+					<li>Types up a short message about his needs and shoots me an email.</li>
+				</ol>
+				<img class="img-responsive" src="../images/personal-site-plan.png" alt="use case flow chart">
+				<h2>Proposed Site Map</h2>
+				<p>Site will consist of 1 page total. No sub-pages or sub-navigation. This will be the Minimum Viable Product. The site will consist of 4 subsections with a sidebar navigation menu.</p>
+				<ul>
+					<li>A Picture of Me, Short Bio, Contact Info/Link to Contact Form</li>
+					<li>Current Projects</li>
+					<li>Past Projects</li>
+					<li>Contact Form</li>
+				</ul>
+				<p><strong>Current Projects: </strong>Detailed description and interactive display of my current project(s). What I plan to accomplish and future goals.</p>
+				<p><strong>Past Projects: </strong>Examples of my past work, including my roles and contributions to each project. Each item will link to the project for demonstration.</p>
+				<p><strong>Contact: </strong>This section will contain a simple contact form, my professional email address, phone number, and additional links to my professional profiles: LinkedIn, and GitHub.</p>
+				<h2>Proposed Features and Functionality</h2>
+				<ul>
+					<li>Top static bar for my contact info.</li>
+					<li>Side static navigation bar for jumping to sections.</li>
+					<li>Contact Form.</li>
+					<li>Iconography using Bootstrap included glyphs.</li>
+				</ul>
+			</main>
 		</div>
 	</body>
 </html>
